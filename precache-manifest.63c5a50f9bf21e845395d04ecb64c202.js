02808cf8995b3aeb96f1dde3a89e6d3ca643cb31
SergijPSO <pankivsergij97@gmail.com>
2019-08-05 23:42:34 +0300

    Updates

---
+++ precache-manifest.63c5a50f9bf21e845395d04ecb64c202.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1a84a081c9fdcba22d364cf8fcff7b",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "8ef05bd1d38a91e5280a",
+    "url": "/react_tabs-with-router/static/css/main.a5b1237d.chunk.css"
+  },
+  {
+    "revision": "0c72d13fac5af47ad76e",
+    "url": "/react_tabs-with-router/static/js/2.75d0a5a5.chunk.js"
+  },
+  {
+    "revision": "8ef05bd1d38a91e5280a",
+    "url": "/react_tabs-with-router/static/js/main.e1f4d368.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
